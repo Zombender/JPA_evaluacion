4b9a1dd1663f07e4b54e8b9557c3d48344e055d8
Johan Reyes <142470650+Zombender@users.noreply.github.com>
2024-10-08 17:58:48 -0600

    Creacion de la clase cliente

---
+++ src/main/java/models/Client.java
@@ -0,0 +1,19 @@
+package models;
+
+package models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+
+public class Client {
+
+}
