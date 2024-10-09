6e82a5b0d272b903de43c590226c450ed73da70d
Francisco2301 <jlopezm@uamv.edu.ni>
2024-10-08 18:13:50 -0600

    continuacion Cliente

--- src/main/java/models/Client.java
+++ src/main/java/models/Client.java
@@ -4,14 +4,32 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.RequiredArgsConstructor;
 
 @Entity
+@Table(name="client")
 @Getter
 @Setter
 @ToString
-@RequiredArgsConstructor
-
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 50)
+    private String fullName;
+    @Column(length = 40)
+    private String email;
+    @Column(length = 8)
+    private String phone;
+    @Column(length = 20)
+    private String date;
 
+    public Client(String date, String phone, String email, String fullName) {
+        this.date = date;
+        this.phone = phone;
+        this.email = email;
+        this.fullName = fullName;
+    }
+    public Client(){
+
+    }
 }
