ef15368ce0cceac5a4655f6e6ba4f1ce8d61056a
Johan Reyes <142470650+Zombender@users.noreply.github.com>
2024-10-08 18:52:08 -0600

    Prueba JPA finalizada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,18 @@
 import dao.ClientDAO;
 import models.Client;
-
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ClientDAO dao = new ClientDAO();
         Client client1 = new Client("Johan Reyes", "jereyesr@uamv.edu.ni","77051013","8/10/24");
         Client client2 = new Client("Ana Martínez", "amartinez@gmail.com", "84567923", "12/09/23");
-        Client client3 = new Client("José López", "jlopezm@uamv.edu.ni", "122345678", "10/05/23");
+        Client client3 = new Client("José López", "jlopezm@uamv.edu.ni", "32345678", "10/05/23");
 
         dao.save(client1);
         dao.save(client2);
         dao.save(client3);
 
-        List<Client> clients = getClients();
+        List<Client> clients = dao.getClients();
         System.out.println("Lista de clientes:");
         for (Client client : clients) {
             System.out.println(client);
@@ -28,7 +28,7 @@ public class Main {
         System.out.println("Cliente eliminado: " + clientToDelete);
 
         System.out.println("Clientes restantes después de eliminar:");
-        List<Client> updatedClients = dao.findAll();
+        List<Client> updatedClients = dao.getClients();
         for (Client client : updatedClients) {
             System.out.println(client);
         }
